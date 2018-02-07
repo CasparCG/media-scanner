a8e302affa4dbcdd7ce64b2fc3d1c431baa19cfe
ronagy <ronagy@icloud.com>
2018-02-07 12:03:23 +0100

    cosmetics

--- src/app.js
+++ src/app.js
@@ -6,103 +6,74 @@ const { getId } = require('./util')
 
 const recursiveReadDirAsync = util.promisify(recursiveReadDir)
 
+const wrap = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
 module.exports = function ({ db, config, logger }) {
   const app = express()
 
   app.use(pinoHttp({ logger }))
 
-  app.get('/cls', async (req, res, next) => {
-    try {
-      const { rows } = await db.allDocs({ include_docs: true })
+  app.get('/cls', wrap(async (req, res) => {
+    const { rows } = await db.allDocs({ include_docs: true })
 
-      const str = rows
-        .map(row => row.doc.cinf || '')
-        .reduce((acc, cinf) => acc + cinf, '')
+    const str = rows
+      .map(row => row.doc.cinf || '')
+      .reduce((acc, cinf) => acc + cinf, '')
 
-      res.send(`200 CLS OK\r\n${str}\r\n`)
-    } catch (err) {
-      next(err)
-    }
-  })
+    res.send(`200 CLS OK\r\n${str}\r\n`)
+  }))
 
-  app.get('/tls', async (req, res, next) => {
-    try {
-      // TODO (perf) Use scanner?
-      let str = ''
-      for (const templatePath of await recursiveReadDirAsync(config.paths.template)) {
-        if (/\.(ft|wt|ct|html)$/.test(templatePath)) {
-          str += `${getId(config.paths.template, templatePath)}\r\n`
-        }
-      }
-      res.send(`200 TLS OK\r\n${str}\r\n`)
-    } catch (err) {
-      next(err)
-    }
-  })
+  app.get('/tls', wrap(async (req, res) => {
+    // TODO (perf) Use scanner?
+    const paths = await recursiveReadDirAsync(config.paths.template)
 
-  app.get('/fls', async (req, res, next) => {
-    try {
-      // TODO (perf) Use scanner?
-      let str = ''
-      for (const fontPath of await recursiveReadDirAsync(config.paths.fonts)) {
-        str += `${getId(config.paths.fonts, fontPath)}\r\n`
-      }
-      res.send(`200 FLS OK\r\n${str}\r\n`)
-    } catch (err) {
-      next(err)
-    }
-  })
+    const str = paths
+      .filter(x => /\.(ft|wt|ct|html)$/.test(x))
+      .reduce((acc, path) => acc + `${getId(config.paths.template, path)}\r\n`, '')
 
-  app.get('/cinf/:id', async (req, res, next) => {
-    try {
-      const { cinf } = await db.get(req.params.id.toUpperCase())
-      res.send(`201 CINF OK\r\n${cinf}`)
-    } catch (err) {
-      next(err)
-    }
-  })
+    res.send(`200 TLS OK\r\n${str}\r\n`)
+  }))
 
-  app.get('/thumbnail/generate', async (req, res, next) => {
-    try {
-      // TODO (fix) Force scanner to scan and wait?
-      res.send(`202 THUMBNAIL GENERATE_ALL OK\r\n`)
-    } catch (err) {
-      next(err)
-    }
-  })
+  app.get('/fls', wrap(async (req, res) => {
+    // TODO (perf) Use scanner?
+    const paths = await recursiveReadDirAsync(config.paths.font)
 
-  app.get('/thumbnail/generate/:id', async (req, res, next) => {
-    try {
-      // TODO (fix) Wait for scanner?
-      res.send(`202 THUMBNAIL GENERATE OK\r\n`)
-    } catch (err) {
-      next(err)
-    }
-  })
+    const str = paths
+      .reduce((acc, path) => acc + `${getId(config.paths.font, path)}\r\n`, '')
 
-  app.get('/thumbnail', async (req, res, next) => {
-    try {
-      const { rows } = await db.allDocs({ include_docs: true })
+    res.send(`200 FLS OK\r\n${str}\r\n`)
+  }))
 
-      const str = rows
-        .map(row => row.doc.tinf || '')
-        .reduce((acc, tinf) => acc + tinf, '')
+  app.get('/cinf/:id', wrap(async (req, res) => {
+    const { cinf } = await db.get(req.params.id.toUpperCase())
+    res.send(`201 CINF OK\r\n${cinf}`)
+  }))
 
-      res.send(`200 THUMBNAIL LIST OK\r\n${str}\r\n`)
-    } catch (err) {
-      next(err)
-    }
-  })
+  app.get('/thumbnail/generate', wrap(async (req, res) => {
+    // TODO (fix) Force scanner to scan and wait?
+    res.send(`202 THUMBNAIL GENERATE_ALL OK\r\n`)
+  }))
 
-  app.get('/thumbnail/:id', async (req, res, next) => {
-    try {
-      const { _attachments } = await db.get(req.params.id.toUpperCase(), { attachments: true })
+  app.get('/thumbnail/generate/:id', wrap(async (req, res) => {
+    // TODO (fix) Wait for scanner?
+    res.send(`202 THUMBNAIL GENERATE OK\r\n`)
+  }))
 
-      res.send(`201 THUMBNAIL RETRIEVE OK\r\n${_attachments['thumb.png'].data}\r\n`)
-    } catch (err) {
-      next(err)
-    }
-  })
+  app.get('/thumbnail', wrap(async (req, res) => {
+    const { rows } = await db.allDocs({ include_docs: true })
+
+    const str = rows
+      .map(row => row.doc.tinf || '')
+      .reduce((acc, tinf) => acc + tinf, '')
+
+    res.send(`200 THUMBNAIL LIST OK\r\n${str}\r\n`)
+  }))
+
+  app.get('/thumbnail/:id', wrap(async (req, res) => {
+    const { _attachments } = await db.get(req.params.id.toUpperCase(), { attachments: true })
+
+    res.send(`201 THUMBNAIL RETRIEVE OK\r\n${_attachments['thumb.png'].data}\r\n`)
+  }))
 
   app.use((err, req, res, next) => {
     req.log.error({ err })
