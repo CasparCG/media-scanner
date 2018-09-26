4c22bea01b11efe887ea1ae9fe28ef11de84f094
Balte de Wit <balte.dewit@gmail.com>
2018-09-26 07:01:13 +0200

    fix: unnecessary rescans on startup

--- src/scanner.js
+++ src/scanner.js
@@ -60,7 +60,9 @@ module.exports = function ({ config, db, logger }) {
       })
       await Promise.all(rows.map(async ({ doc }) => {
         try {
-          if (doc.mediaPath.indexOf(config.scanner.paths) === 0) {
+          const mediaFolder = config.scanner.paths.split('\\').join('/')
+          const mediaPath = doc.mediaPath.split('\\').join('/')
+          if (mediaPath.indexOf(mediaFolder) === 0) {
             try {
               const stat = await statAsync(doc.mediaPath)
               if (stat.isFile()) {
